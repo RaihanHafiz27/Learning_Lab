1acae7e14beaa567640747918c69a2b9a25392fe
M Raihan Hafiz <muhammadraihan27072@gmail.com>
2026-01-05 21:28:56 +0700

    learn : looping

---
+++ src/looping/classics/index.js
@@ -0,0 +1,11 @@
+// For Loop
+for (var i = 0; i < 5; i++) {
+    console.log("Iteration -> ".concat(i));
+}
+console.log("=============== BATAS ===============");
+// While Loop
+var energy = 3;
+while (energy > 0) {
+    console.log("Can Running!");
+    energy--;
+}
